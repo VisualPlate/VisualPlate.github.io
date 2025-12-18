8853654f78000d3cd1fef8374b431673ae21ebe7
VisualPlate <tolonenjere0@gmail.com>
2025-12-18 17:03:46 +0200

    added pages and made mobile navigation

---
+++ assets/js/nav.js
@@ -0,0 +1,19 @@
+const mobNav = document.getElementById("mobile-nav-show")
+const openNav = document.getElementById("open-mobile-nav") 
+
+openNav.addEventListener("click", function(){
+    if (mobNav.style.display == "none") {
+        mobNav.style.display = "flex";
+    } else {
+        mobNav.style.display = "none"
+    }
+});
+
+window.addEventListener("resize", function() {
+    const curScreenWidth = window.innerWidth;
+    //In pixels. value at what point to hide mobile nav
+    const whenHide = 800;
+    if (curScreenWidth > whenHide) {
+        mobNav.style.display = "none";
+    }
+})
